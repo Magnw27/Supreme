dbd7e13f689ea0587baa0173560358e37f9c3eca
Magnw27 <ripzzbg@gmail.com>
2026-06-21 21:48:48 +0700

    Update resource core item names to English

--- src/main/java/com/github/relativobr/supreme/resource/core/SupremeCoreBlock.java
+++ src/main/java/com/github/relativobr/supreme/resource/core/SupremeCoreBlock.java
@@ -10,56 +10,56 @@ import org.bukkit.Material;
 public class SupremeCoreBlock {
 
   public static final SlimefunItemStack RESOURCE_CORE_STONE = new SupremeItemStack("SUPREME_CORE_STONE",
-      "454d9c488c3fbde5454e38619f9cc5b5ba8c6c0186f8aa1da60900fcbc3ea6", "&a至尊圆石", "",
-      "&7超级圆石", "", "&3至尊核心合成材料");
+      "454d9c488c3fbde5454e38619f9cc5b5ba8c6c0186f8aa1da60900fcbc3ea6", "&aSupreme Cobblestone", "",
+      "&7Super Cobblestone", "", "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_STONE = new CustomCoreRecipe(RESOURCE_CORE_STONE,
       Material.COBBLESTONE);
 
   public static final SlimefunItemStack RESOURCE_CORE_GRANITE = new SupremeItemStack("SUPREME_CORE_GRANITE",
-      "a0285bea3c8a02db139fa8ec5cc588615a98550725f8e676c93fdbc33b6b", "&a至尊花岗岩", "", "&7超级花岗岩", "",
-      "&3至尊核心合成材料");
+      "a0285bea3c8a02db139fa8ec5cc588615a98550725f8e676c93fdbc33b6b", "&aSupreme Granite", "", "&7Super Granite", "",
+      "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_GRANITE = new CustomCoreRecipe(RESOURCE_CORE_GRANITE,
       Material.GRANITE);
 
   public static final SlimefunItemStack RESOURCE_CORE_DIORITE = new SupremeItemStack("SUPREME_CORE_DIORITE",
-      "13fa5265a336abde301a9d59af4783e82a10dad0817716ead2962ab7c6d3dff", "&a至尊闪长岩", "", "&7超级闪长岩",
-      "", "&3至尊核心合成材料");
+      "13fa5265a336abde301a9d59af4783e82a10dad0817716ead2962ab7c6d3dff", "&aSupreme Diorite", "", "&7Super Diorite",
+      "", "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_DIORITE = new CustomCoreRecipe(RESOURCE_CORE_DIORITE,
       Material.DIORITE);
 
   public static final SlimefunItemStack RESOURCE_CORE_ANDESITE = new SupremeItemStack("SUPREME_CORE_ANDESITE",
-      "adb7bf059a62d27b1e1e2f34394f3f38ed8cda45471f6f4d5b47c3912d181135", "&a至尊安山岩", "",
-      "&7超级安山岩", "", "&3至尊核心合成材料");
+      "adb7bf059a62d27b1e1e2f34394f3f38ed8cda45471f6f4d5b47c3912d181135", "&aSupreme Andesite", "",
+      "&7Super Andesite", "", "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_ANDESITE = new CustomCoreRecipe(RESOURCE_CORE_ANDESITE,
       Material.ANDESITE);
 
   public static final SlimefunItemStack RESOURCE_CORE_GRAVEL = new SupremeItemStack("SUPREME_CORE_GRAVEL",
-      "7788b61fcd46cad9235cc32ac57a54f708110329447bb24c0877886e3f4907f1", "&a至尊沙砾", "", "&7超级沙砾",
-      "", "&3至尊核心合成材料");
+      "7788b61fcd46cad9235cc32ac57a54f708110329447bb24c0877886e3f4907f1", "&aSupreme Gravel", "", "&7Super Gravel",
+      "", "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_GRAVEL = new CustomCoreRecipe(RESOURCE_CORE_GRAVEL,
       Material.GRAVEL);
 
   public static final SlimefunItemStack RESOURCE_CORE_SAND = new SupremeItemStack("SUPREME_CORE_SAND",
-      "53398ab3cb696b34430be944b14afbd227fd87e99026bcfc8b7387a861bde", "&a至尊沙子", "", "&7超级沙子", "",
-      "&3至尊核心合成材料");
+      "53398ab3cb696b34430be944b14afbd227fd87e99026bcfc8b7387a861bde", "&aSupreme Sand", "", "&7Super Sand", "",
+      "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_SAND = new CustomCoreRecipe(RESOURCE_CORE_SAND,
       Material.SAND);
 
   public static final SlimefunItemStack RESOURCE_CORE_ENDSTONE = new SupremeItemStack("SUPREME_CORE_ENDSTONE",
-      "19f21f5d883316fd65a9366f32a33013182e3381dec21c17c78355d9bf4f0", "&a至尊末地石", "", "&7超级末地石",
-      "", "&3至尊核心合成材料");
+      "19f21f5d883316fd65a9366f32a33013182e3381dec21c17c78355d9bf4f0", "&aSupreme End Stone", "", "&7Super End Stone",
+      "", "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_ENDSTONE = new CustomCoreRecipe(RESOURCE_CORE_ENDSTONE,
       Material.END_STONE);
 
   public static final SlimefunItemStack RESOURCE_CORE_CLAY = new SupremeItemStack("SUPREME_CORE_CLAY",
-      "67826829eab5ad62f0c11d9faafdc9954364871160dd839e1ab5a3b213a33", "&a至尊粘土", "", "&7超级粘土", "",
-      "&3至尊核心合成材料");
+      "67826829eab5ad62f0c11d9faafdc9954364871160dd839e1ab5a3b213a33", "&aSupreme Clay", "", "&7Super Clay", "",
+      "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_CLAY = new CustomCoreRecipe(RESOURCE_CORE_CLAY,
       Material.CLAY);
 
   public static final SlimefunItemStack RESOURCE_CORE_SNOW = new SupremeItemStack("SUPREME_CORE_SNOW",
-      "5dd6fe267a418dcc7f37a8f76855b5328b1303897b342a107cf162f14fe3d", "&a至尊雪", "", "&7超级雪", "",
-      "&3至尊核心合成材料");
+      "5dd6fe267a418dcc7f37a8f76855b5328b1303897b342a107cf162f14fe3d", "&aSupreme Snow", "", "&7Super Snow", "",
+      "&3Supreme Core Crafting Material");
   public static final CustomCoreRecipe RECIPE_RESOURCE_CORE_SNOW = new CustomCoreRecipe(RESOURCE_CORE_SNOW,
       Material.SNOW_BLOCK);
 }
