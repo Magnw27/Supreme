da0ace13665235e1eab3cf54fb47e2039af58b60
Magnw27 <ripzzbg@gmail.com>
2026-06-21 21:19:17 +0700

    Update recipe requirements to English

--- src/main/java/com/github/relativobr/supreme/machine/recipe/MobCollectorMachineRecipe.java
+++ src/main/java/com/github/relativobr/supreme/machine/recipe/MobCollectorMachineRecipe.java
@@ -33,92 +33,92 @@ public class MobCollectorMachineRecipe extends MachineRecipe implements Predicat
     SupremeOptions supremeOptions = getSupremeOptions();
 
     List<ItemStack> displayRecipes = new ArrayList();
-    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&f需要&b蜜蜂&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&fRequires &bbee&f nearby"));
     displayRecipes.add(new ItemStack(Material.HONEY_BOTTLE));
-    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&f需要&b鱿鱼&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&fRequires &bsquid&f nearby"));
     displayRecipes.add(new ItemStack(Material.INK_SAC));
-    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&f需要&b荧光鱿鱼&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&fRequires &bglow squid&f nearby"));
     displayRecipes.add(new ItemStack(Material.GLOW_INK_SAC));
-    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&f需要&b凋灵&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&fRequires &bwither&f nearby"));
     displayRecipes.add(new ItemStack(Material.EXPERIENCE_BOTTLE));
-    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&f需要&b末影龙&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.GLASS_BOTTLE, null, "&fRequires &bender dragon&f nearby"));
     displayRecipes.add(new ItemStack(Material.DRAGON_BREATH));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b蜜蜂&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bbee&f nearby"));
     displayRecipes.add(new ItemStack(Material.HONEYCOMB));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b羊&f在附近(有颜色的)"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bsheep&f nearby (colored)"));
     displayRecipes.add(new ItemStack(Material.WHITE_WOOL));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b牛&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bcow&f nearby"));
     displayRecipes.add(new ItemStack(Material.LEATHER));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b鸡&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bchicken&f nearby"));
     displayRecipes.add(new ItemStack(Material.FEATHER));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b守卫者&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bguardian&f nearby"));
     displayRecipes.add(new ItemStack(Material.SPONGE));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b蜘蛛&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bspider&f nearby"));
     displayRecipes.add(new ItemStack(Material.SPIDER_EYE));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b凋灵骷髅&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bwither skeleton&f nearby"));
     displayRecipes.add(new ItemStack(Material.COAL));
-    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b凋灵&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &bwither&f nearby"));
     displayRecipes.add(SlimefunItems.COMPRESSED_CARBON);
     if (!supremeOptions.isCustomBc()) {
-      displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&f需要&b铁傀儡&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.SHEARS, null, "&fRequires &biron golem&f nearby"));
       displayRecipes.add(SlimefunItems.BASIC_CIRCUIT_BOARD);
     }
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b幻翼&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bphantom&f nearby"));
     displayRecipes.add(new ItemStack(Material.PHANTOM_MEMBRANE));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b僵尸&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bzombie&f nearby"));
     displayRecipes.add(new ItemStack(Material.ROTTEN_FLESH));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b骷髅&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bskeleton&f nearby"));
     displayRecipes.add(new ItemStack(Material.BONE));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b爬行者&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bcreeper&f nearby"));
     displayRecipes.add(new ItemStack(Material.GUNPOWDER));
     if (!supremeOptions.isCustomBc()) {
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b史莱姆&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bslime&f nearby"));
       displayRecipes.add(new ItemStack(Material.SLIME_BALL));
     }
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b蜘蛛&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bspider&f nearby"));
     displayRecipes.add(new ItemStack(Material.STRING));
     if (!supremeOptions.isCustomBc()) {
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b凋灵骷髅&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bwither skeleton&f nearby"));
       displayRecipes.add(new ItemStack(Material.WITHER_SKELETON_SKULL));
     }
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b末影人&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &benderman&f nearby"));
     displayRecipes.add(new ItemStack(Material.ENDER_PEARL));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b烈焰人&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bblaze&f nearby"));
     displayRecipes.add(new ItemStack(Material.BLAZE_ROD));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b岩浆怪&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bmagma cube&f nearby"));
     displayRecipes.add(new ItemStack(Material.MAGMA_CREAM));
     if (!supremeOptions.isCustomBc()) {
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b凋灵&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bwither&f nearby"));
       displayRecipes.add(new ItemStack(Material.NETHER_STAR));
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b恶魂&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bghast&f nearby"));
       displayRecipes.add(new ItemStack(Material.GHAST_TEAR));
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b劫掠兽&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bravager&f nearby"));
       displayRecipes.add(new ItemStack(Material.TOTEM_OF_UNDYING));
     }
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b牛&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bcow&f nearby"));
     displayRecipes.add(new ItemStack(Material.BEEF));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b猪&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bpig&f nearby"));
     displayRecipes.add(new ItemStack(Material.PORKCHOP));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b鸡&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bchicken&f nearby"));
     displayRecipes.add(new ItemStack(Material.CHICKEN));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b羊&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bsheep&f nearby"));
     displayRecipes.add(new ItemStack(Material.MUTTON));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b雪傀儡&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bsnow golem&f nearby"));
     displayRecipes.add(new ItemStack(Material.SNOWBALL));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b铁傀儡&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &biron golem&f nearby"));
     displayRecipes.add(new ItemStack(Material.IRON_INGOT));
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b溺尸&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bdrowned&f nearby"));
     displayRecipes.add(new ItemStack(Material.NAUTILUS_SHELL));
     if (!supremeOptions.isCustomBc()) {
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b守卫者&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bguardian&f nearby"));
       displayRecipes.add(new ItemStack(Material.PRISMARINE_SHARD));
-      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b远古守卫者&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &belder guardian&f nearby"));
       displayRecipes.add(new ItemStack(Material.PRISMARINE_CRYSTALS));
     }
-    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&f需要&b凋灵&f在附近"));
+    displayRecipes.add(new CustomItemStack(Material.IRON_SWORD, null, "&fRequires &bwither&f nearby"));
     displayRecipes.add(new ItemStack(Material.GLASS_BOTTLE));
     if (!supremeOptions.isCustomBc()) {
-      displayRecipes.add(new CustomItemStack(Material.GOLD_INGOT, null, "&f需要&b猪灵&f在附近"));
+      displayRecipes.add(new CustomItemStack(Material.GOLD_INGOT, null, "&fRequires &bpiglin&f nearby"));
       displayRecipes.add(SlimefunItems.STRANGE_NETHER_GOO);
     }
     return displayRecipes;
