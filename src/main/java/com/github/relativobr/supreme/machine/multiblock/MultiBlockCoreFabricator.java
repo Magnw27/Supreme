04d3c7d5951f6483c09ba0e777800f3818007d49
Magnw27 <ripzzbg@gmail.com>
2026-06-21 21:15:44 +0700

    Update CORE_FABRICATOR item details to English

--- src/main/java/com/github/relativobr/supreme/machine/multiblock/MultiBlockCoreFabricator.java
+++ src/main/java/com/github/relativobr/supreme/machine/multiblock/MultiBlockCoreFabricator.java
@@ -27,7 +27,7 @@ import org.bukkit.inventory.ItemStack;
 public class MultiBlockCoreFabricator extends MultiBlockMachine implements NotPlaceable {
 
   public static final SlimefunItemStack CORE_FABRICATOR = new SupremeItemStack("SUPREME_MULTIBLOCK_CORE",
-      Material.SHROOMLIGHT, "&e核心工厂", "", "&7&o你可以在这制造核心！", "", "&a多方块机器");
+        Material.SHROOMLIGHT, "&eCore Factory", "", "&7&oYou can craft cores here!", "", "&aMultiblock Machine");
   public static final RecipeType MACHINE_CORE_FABRICATOR = new RecipeType(
       new NamespacedKey(Supreme.inst(), "SUPREME_MULTIBLOCK_CORE_KEY"), CORE_FABRICATOR);
 
