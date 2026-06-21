9508ef18dd4df7bf81500958a7b18e1c7f486b1d
Magnw27 <ripzzbg@gmail.com>
2026-06-21 21:41:39 +0700

    Update MobCollector localization and descriptions

--- src/main/java/com/github/relativobr/supreme/machine/MobCollector.java
+++ src/main/java/com/github/relativobr/supreme/machine/MobCollector.java
@@ -77,27 +77,27 @@ import org.springframework.scheduling.annotation.Async;
 public class MobCollector extends SimpleItemWithLargeContainerMachine {
 
   public static final SlimefunItemStack MOB_COLLECTOR_MACHINE = new SupremeItemStack("SUPREME_MOB_COLLECTOR_MACHINE_I",
-      Material.RESPAWN_ANCHOR, "&b生物收集器", "", "&f这个机器允许你收集",
-      "&f附近生物的战利品(4格)", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE),
-      LoreBuilder.speed(1), LoreBuilder.powerBuffer(1000), LoreBuilder.powerPerSecond(20), "", "&3至尊机器");
+      Material.RESPAWN_ANCHOR, "&bMob Collector", "", "&fThis machine allows you to collect",
+      "&floot from nearby mobs (4 blocks)", "", LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE),
+      LoreBuilder.speed(1), LoreBuilder.powerBuffer(1000), LoreBuilder.powerPerSecond(20), "", "&3Supreme Machine");
   public static final ItemStack[] RECIPE_MOB_COLLECTOR_MACHINE = new ItemStack[]{SupremeComponents.RUSTLESS_MACHINE,
       new ItemStack(Material.RESPAWN_ANCHOR), SupremeComponents.RUSTLESS_MACHINE, SupremeComponents.INDUCTIVE_MACHINE,
       SupremeComponents.SYNTHETIC_RUBY, SupremeComponents.INDUCTIVE_MACHINE, SupremeComponents.ADAMANTIUM_PLATE,
       SlimefunItems.PROGRAMMABLE_ANDROID_3_BUTCHER, SupremeComponents.ADAMANTIUM_PLATE};
 
   public static final SlimefunItemStack MOB_COLLECTOR_MACHINE_II = new SupremeItemStack(
-      "SUPREME_MOB_COLLECTOR_MACHINE_II", Material.RESPAWN_ANCHOR, "&b生物收集器II", "",
-      "&f这个机器允许你收集", "&f附近生物的战利品(8格)", "",
+      "SUPREME_MOB_COLLECTOR_MACHINE_II", Material.RESPAWN_ANCHOR, "&bMob Collector II", "",
+      "&fThis machine allows you to collect", "&floot from nearby mobs (8 blocks)", "",
       LoreBuilder.machine(MachineTier.END_GAME, MachineType.MACHINE), LoreBuilder.speed(5),
-      LoreBuilder.powerBuffer(5000), LoreBuilder.powerPerSecond(100), "", "&3至尊机器");
+      LoreBuilder.powerBuffer(5000), LoreBuilder.powerPerSecond(100), "", "&3Supreme Machine");
   public static final ItemStack[] RECIPE_MOB_COLLECTOR_MACHINE_II = new ItemStack[]{
       SupremeComponents.CONVEYANCE_MACHINE, SupremeCetrus.CETRUS_LUMIUM, SupremeComponents.CONVEYANCE_MACHINE,
       SupremeComponents.INDUCTOR_MACHINE, MobCollector.MOB_COLLECTOR_MACHINE, SupremeComponents.INDUCTOR_MACHINE,
       SupremeComponents.THORNERITE, SupremeCetrus.CETRUS_IGNIS, SupremeComponents.THORNERITE};
 
   public static final SlimefunItemStack MOB_COLLECTOR_MACHINE_III = new SupremeItemStack(
-      "SUPREME_MOB_COLLECTOR_MACHINE_III", Material.RESPAWN_ANCHOR, "&b生物收集器III", "",
-      "&f这个机器允许你收集", "&f附近生物的战利品(16格)", "",
+      "SUPREME_MOB_COLLECTOR_MACHINE_III", Material.RESPAWN_ANCHOR, "&bMob Collector III", "",
+      "&fThis machine allows you to collect", "&floot from nearby mobs (16 blocks)", "",
       LoreBuilder.machine(MachineTier.END_GAME, MachineType.MACHINE), LoreBuilder.speed(15),
       LoreBuilder.powerBuffer(15000), LoreBuilder.powerPerSecond(300), "", "&3至尊机器");
   public static final ItemStack[] RECIPE_MOB_COLLECTOR_MACHINE_III = new ItemStack[]{SupremeComponents.THORNERITE,
@@ -330,7 +330,7 @@ public class MobCollector extends SimpleItemWithLargeContainerMachine {
   @Nonnull
   @Override
   public String getRecipeSectionLabel(@Nonnull Player p) {
-    return "&7收集:";
+    return "&7Collected:";
   }
 
 
